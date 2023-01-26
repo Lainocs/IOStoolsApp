9e000940ee1742893880d923eab788029b7df6f4
Nicolas de Garrigues <ndegarrigues@gmail.com>
2023-01-26 13:44:17 +0100

    Initial Commit

---
+++ deGarriguesNicolasSwift/deGarriguesNicolasSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  deGarriguesNicolasSwiftApp.swift
+//  deGarriguesNicolasSwift
+//
+//  Created by Nicolas de Garrigues on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct deGarriguesNicolasSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
